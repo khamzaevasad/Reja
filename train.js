8177f6c0c4db0072dfab0d2dec1d0d0d8ad285f8
khamzaevasad <xamzayevasad442@gmail.com>
2025-10-01 12:05:22 +0900

    D-task

--- train.js
+++ train.js
@@ -118,8 +118,67 @@
 /* Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
 MASALAN countLetter("e", "engineer") 3ni return qiladi. */
 
-function countLetter(a, b) {
-  const result = b.split("").filter((item) => item === a).length;
-  return result;
+// function countLetter(a, b) {
+//   const result = b.split("").filter((item) => item === a).length;
+//   return result;
+// }
+// console.log(countLetter("e", "leo"));
+
+// D-TASK:
+/*
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+*/
+
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.products = {
+      non,
+      lagmon,
+      cola,
+    };
+  }
+
+  getTime() {
+    const now = new Date();
+    const hh = String(now.getHours()).padStart(2, "0");
+    const mm = String(now.getMinutes()).padStart(2, "0");
+    return `${hh}:${mm}`;
+  }
+
+  qoldiq() {
+    const t = this.getTime();
+    console.log(
+      `hozir ${t}da ${this.products.non}ta non, ${this.products.lagmon}ta lagmon va ${this.products.cola}ta cola mavjud!`
+    );
+  }
+
+  sotish(nomi, soni) {
+    if (this.products[nomi] === undefined) {
+      console.log(`Bunday mahsulot yo‘q: ${nomi}`);
+      return;
+    }
+    if (this.products[nomi] < soni) {
+      console.log(`Hozircha yetarli ${nomi} yo‘q!`);
+      return;
+    }
+    this.products[nomi] -= soni;
+    console.log(`${this.getTime()} da ${soni}ta ${nomi} sotildi.`);
+  }
+
+  qabul(nomi, soni) {
+    if (this.products[nomi] === undefined) {
+      console.log(`Bunday mahsulot yo‘q: ${nomi}`);
+      return;
+    }
+    this.products[nomi] += soni;
+    console.log(`${this.getTime()} da ${soni}ta ${nomi} qabul qilindi.`);
+  }
 }
-console.log(countLetter("e", "leo"));
+
+const shop = new Shop(4, 5, 2);
+
+shop.qoldiq();
+shop.sotish("non", 3);
+shop.qabul("cola", 4);
+shop.qoldiq();
